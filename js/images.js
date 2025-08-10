14501022dffaeacb026b2aec30181635ed974537
AEAdmin <contact@codepixie.de>
2025-08-10 19:50:49 +0200

    New images for the challenge made by me.

--- js/images.js
+++ js/images.js
@@ -1,43 +1,38 @@
 export const images = [
     {
         id: 1,
-        img: 'images/test.png',
-        authorUrl: 'https://codepixie.de',
-        author: 'codepixie'
+        img: 'images/cat-with-clothes.jpg',
+        authorUrl: 'https://www.instagram.com/p/CiVqrE-qEWK/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
+        author: 'anmadraws'
     }, { 
         id: 2, 
-        img: 'images/test.png', 
-        authorUrl: 'https://codepixie.de', 
-        author: 'codepixie' 
+        img: 'images/frog-on-mushroom.jpg', 
+        authorUrl: 'https://www.instagram.com/p/CTW3RwOjUci/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==', 
+        author: 'anmadraws' 
     }, { 
         id: 3,
-         img: 'images/test.png', 
-         authorUrl: 'https://codepixie.de', 
-         author: 'codepixie' 
+         img: 'images/goldfish.jpg', 
+         authorUrl: 'https://www.instagram.com/p/CEW3iq_A8Ic/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==', 
+         author: 'anmadraws' 
     }, { 
         id: 4,        
-        img: 'images/test.png', 
-        authorUrl: 'https://codepixie.de', 
-        author: 'codepixie' 
+        img: 'images/rabbit-pencil-water-color.jpg', 
+        authorUrl: 'https://www.instagram.com/p/CD7BJ4SAoZT/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==', 
+        author: 'anmadraws' 
     }, { 
         id: 5, 
-        img: 'images/test.png', 
-        authorUrl: 'https://codepixie.de', 
-        author: 'codepixie' 
+        img: 'images/raven.jpg', 
+        authorUrl: 'https://www.instagram.com/p/CFFtAKSgTfr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==', 
+        author: 'anmadraws' 
     }, { 
         id: 6, 
-        img: 'images/test.png', 
-        auauthorUrlthor: 'https://codepixie.de', 
-        author: 'codepixie' },
+        img: 'images/red-rabbit.jpg', 
+        auauthorUrlthor: 'https://www.instagram.com/p/CQ6qgTgL2sa/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==', 
+        author: 'anmadraws' },
     { 
         id: 7, 
-        img: 'images/test.png', 
-        authorUrl: 'https://codepixie.de', 
-        author: 'codepixie' 
-    }, { 
-        id: 8, 
-        img: 'images/test.png', 
-        authorUrl: 'https://codepixie.de', 
-        author: 'codepixie' 
+        img: 'images/squirrel-water-color.jpg', 
+        authorUrl: 'https://www.instagram.com/anmadraws/', 
+        author: 'anmadraws' 
     }
 ];
